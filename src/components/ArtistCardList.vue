b33ae0157dde1a6e1a9093c1100f0b17999e4278
krthr <twilson@uninorte.edu.co>
2019-11-20 13:08:42 -0500

    Artist card list

---
+++ src/components/ArtistCardList.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <v-col cols="6" sm="3" md="2" v-for="i of 4" :key="i">
+      <artist-card></artist-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "ArtistCardList",
+
+  components: {
+    ArtistCard: () => import("@/components/ArtistCard.vue")
+  }
+};
+</script>
