0d7cc9b8fa80e51ac0c615749b2f3a66ac84f56a
krthr <twilson@uninorte.edu.co>
2019-11-20 13:09:09 -0500

    Song list

---
+++ src/components/SongList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <song-list-item v-for="i of total" :key="i" dense></song-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SongList",
+
+  props: {
+    total: {
+      type: Number,
+      default: 5
+    }
+  },
+
+  components: {
+    SongListItem: () => import("@/components/SongListItem.vue")
+  }
+};
+</script>
