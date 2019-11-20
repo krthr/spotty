6c5846c3c2d498dcf4eb977989e6523876ac9f9f
krthr <twilson@uninorte.edu.co>
2019-11-20 13:10:55 -0500

    Queue page

---
+++ src/views/Queue.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container id="playlist" fluid>
+    <v-row>
+      <v-col>
+        <h1>Play Queue</h1>
+
+        <v-subheader>ACTUAL</v-subheader>
+
+        <song-list total="1"></song-list>
+
+        <v-list style="background: none">
+          <v-subheader>NEXT</v-subheader>
+
+          <song-list></song-list>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Queue",
+
+  components: {
+    SongList: () => import("@/components/SongList.vue")
+  }
+};
+</script>
