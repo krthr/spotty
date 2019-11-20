6e6ab02ccd894c40a8930c088bad0ade5cd63fca
krthr <twilson@uninorte.edu.co>
2019-11-20 13:08:36 -0500

    Artist card

---
+++ src/components/ArtistCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-hover>
+    <template v-slot:default="{ hover }">
+      <v-card class="artist-card" flat>
+        <v-card-text>
+          <v-img
+            class="elevation-10"
+            src="https://direct.rhapsody.com/imageserver/images/alb.261770523/500x500.jpg"
+            style="border-radius: 50%"
+          >
+          </v-img>
+        </v-card-text>
+
+        <v-card-text class="pt-0">
+          <p class="body-2 font-weight-medium ma-0 text-truncate white--text">
+            Army of Bones
+          </p>
+          <p class="caption font-weight-light mb-4 text-truncate">
+            Artist
+          </p>
+        </v-card-text>
+
+        <v-overlay absolute opacity="0" :value="hover">
+          <v-icon size="35">mdi-play-circle-outline</v-icon>
+        </v-overlay>
+      </v-card>
+    </template>
+  </v-hover>
+</template>
