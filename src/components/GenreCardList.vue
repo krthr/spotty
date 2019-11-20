84c6dd3b8f9b5214a767bdf975998ad8b98e7830
krthr <twilson@uninorte.edu.co>
2019-11-20 13:08:58 -0500

    Genre card list

---
+++ src/components/GenreCardList.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row>
+    <v-col cols="6" sm="3" md="2" v-for="i of total" :key="i">
+      <genre-card></genre-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "GenreCardList",
+
+  props: {
+    total: {
+      type: Number,
+      default: 5
+    }
+  },
+
+  components: {
+    GenreCard: () => import("@/components/GenreCard.vue")
+  }
+};
+</script>
