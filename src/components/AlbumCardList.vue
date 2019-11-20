43381f3e3f6cb19b33b979b8b13044646ac00d5c
krthr <twilson@uninorte.edu.co>
2019-11-20 13:08:29 -0500

    Album card list

---
+++ src/components/AlbumCardList.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <v-col cols="6" sm="3" md="2" v-for="i of 4" :key="i">
+      <album-card></album-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "AlbumCardList",
+
+  components: {
+    AlbumCard: () => import("@/components/AlbumCard.vue")
+  }
+};
+</script>
