1e41098639b33127e68033406e6d96b681e957d4
krthr <twilson@uninorte.edu.co>
2019-11-22 18:39:28 -0500

    init

---
+++ precache-manifest.22d06d4be625a6ba8b0d504cb071dfbc.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791bd558c09c8b2b2fd5",
+    "url": "/spotty/css/app-layout.4080f4e0.css"
+  },
+  {
+    "revision": "855958e4e0380c7f657f",
+    "url": "/spotty/css/app.595c8a63.css"
+  },
+  {
+    "revision": "fec1415608ee276dd834",
+    "url": "/spotty/css/auth-layout.3acc406a.css"
+  },
+  {
+    "revision": "d46511852a7af071aaab",
+    "url": "/spotty/css/chunk-02229283.9e1d767f.css"
+  },
+  {
+    "revision": "3df006cec9326d84380d",
+    "url": "/spotty/css/chunk-0ec9a4c0.b18c2492.css"
+  },
+  {
+    "revision": "2601b96487a163eef30d",
+    "url": "/spotty/css/chunk-0ffe3c7a.92bf7ec4.css"
+  },
+  {
+    "revision": "6871f3b6be3a4b724ffc",
+    "url": "/spotty/css/chunk-13af94db.002949db.css"
+  },
+  {
+    "revision": "2e1eefb0dbde7913c86e",
+    "url": "/spotty/css/chunk-28e5c734.1f0d52e7.css"
+  },
+  {
+    "revision": "671b62a218b7a3344c37",
+    "url": "/spotty/css/chunk-315b6b0c.c1376f4a.css"
+  },
+  {
+    "revision": "9b0f51dc2201aa64cb69",
+    "url": "/spotty/css/chunk-36167c0f.3416000f.css"
+  },
+  {
+    "revision": "13f8fffd7e2ba7ed2c37",
+    "url": "/spotty/css/chunk-3fe16efc.b6a1356c.css"
+  },
+  {
+    "revision": "a5ba313936a513a8badd",
+    "url": "/spotty/css/chunk-5d85aaba.ca409b1e.css"
+  },
+  {
+    "revision": "02237a282da90119fb10",
+    "url": "/spotty/css/chunk-62f9cff0.2cb43bd7.css"
+  },
+  {
+    "revision": "3f0b364f9773d45bf81f",
+    "url": "/spotty/css/chunk-6be6bf44.ec7374bb.css"
+  },
+  {
+    "revision": "b0870072b0b91253da37",
+    "url": "/spotty/css/chunk-72b14d6a.80ece456.css"
+  },
+  {
+    "revision": "3a3d17c5b370072c4d69",
+    "url": "/spotty/css/chunk-b45418b4.1f0d52e7.css"
+  },
+  {
+    "revision": "6bc35e15073f88f58962",
+    "url": "/spotty/css/chunk-f0e3b72e.6059396f.css"
+  },
+  {
+    "revision": "653e7c12d9fa26673815",
+    "url": "/spotty/css/chunk-vendors.92cc8cd5.css"
+  },
+  {
+    "revision": "4feb15049364c5a0fef1",
+    "url": "/spotty/css/login.431a8a3e.css"
+  },
+  {
+    "revision": "ef308f345d14f3ca1f41211c5cc72414",
+    "url": "/spotty/index.html"
+  },
+  {
+    "revision": "791bd558c09c8b2b2fd5",
+    "url": "/spotty/js/app-layout.7cea34ae.js"
+  },
+  {
+    "revision": "855958e4e0380c7f657f",
+    "url": "/spotty/js/app.f9cb5029.js"
+  },
+  {
+    "revision": "fec1415608ee276dd834",
+    "url": "/spotty/js/auth-layout.6ce1c777.js"
+  },
+  {
+    "revision": "d46511852a7af071aaab",
+    "url": "/spotty/js/chunk-02229283.0fc5ac6b.js"
+  },
+  {
+    "revision": "3df006cec9326d84380d",
+    "url": "/spotty/js/chunk-0ec9a4c0.0e686229.js"
+  },
+  {
+    "revision": "2601b96487a163eef30d",
+    "url": "/spotty/js/chunk-0ffe3c7a.06f6d077.js"
+  },
+  {
+    "revision": "6871f3b6be3a4b724ffc",
+    "url": "/spotty/js/chunk-13af94db.db239fed.js"
+  },
+  {
+    "revision": "2e1eefb0dbde7913c86e",
+    "url": "/spotty/js/chunk-28e5c734.8e52ba26.js"
+  },
+  {
+    "revision": "4a0acf4d38189df3069c",
+    "url": "/spotty/js/chunk-2904ea9b.b17cdd38.js"
+  },
+  {
+    "revision": "aab6b4da33c1db7b1680",
+    "url": "/spotty/js/chunk-2d0cc3fb.059a6b50.js"
+  },
+  {
+    "revision": "1bc97cda3a7e9d848f34",
+    "url": "/spotty/js/chunk-2d0d7466.fb0b240e.js"
+  },
+  {
+    "revision": "db7e8415e5266bd78593",
+    "url": "/spotty/js/chunk-2d0db846.f0f33bd9.js"
+  },
+  {
+    "revision": "d7d2312685c2d47d590f",
+    "url": "/spotty/js/chunk-2d0de1c0.d81e9e2a.js"
+  },
+  {
+    "revision": "e2358d7f864fabf8e8cc",
+    "url": "/spotty/js/chunk-2d2221b9.0171c1e7.js"
+  },
+  {
+    "revision": "671b62a218b7a3344c37",
+    "url": "/spotty/js/chunk-315b6b0c.f92e651b.js"
+  },
+  {
+    "revision": "9b0f51dc2201aa64cb69",
+    "url": "/spotty/js/chunk-36167c0f.a3ef1989.js"
+  },
+  {
+    "revision": "13f8fffd7e2ba7ed2c37",
+    "url": "/spotty/js/chunk-3fe16efc.1c07a8ad.js"
+  },
+  {
+    "revision": "8056cc260567e5af6ef2",
+    "url": "/spotty/js/chunk-4ea93d74.7ab076c3.js"
+  },
+  {
+    "revision": "a5ba313936a513a8badd",
+    "url": "/spotty/js/chunk-5d85aaba.46b56e1c.js"
+  },
+  {
+    "revision": "02237a282da90119fb10",
+    "url": "/spotty/js/chunk-62f9cff0.51df90fe.js"
+  },
+  {
+    "revision": "3f0b364f9773d45bf81f",
+    "url": "/spotty/js/chunk-6be6bf44.beb6644c.js"
+  },
+  {
+    "revision": "b0870072b0b91253da37",
+    "url": "/spotty/js/chunk-72b14d6a.f9393bdb.js"
+  },
+  {
+    "revision": "3a3d17c5b370072c4d69",
+    "url": "/spotty/js/chunk-b45418b4.2c10c77b.js"
+  },
+  {
+    "revision": "6bc35e15073f88f58962",
+    "url": "/spotty/js/chunk-f0e3b72e.7feacfb9.js"
+  },
+  {
+    "revision": "653e7c12d9fa26673815",
+    "url": "/spotty/js/chunk-vendors.613b8fb5.js"
+  },
+  {
+    "revision": "4feb15049364c5a0fef1",
+    "url": "/spotty/js/login.6c9a960a.js"
+  },
+  {
+    "revision": "c187e97026316b944fd653ba944d4a90",
+    "url": "/spotty/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/spotty/robots.txt"
+  },
+  {
+    "revision": "a38d7c0ad57bb85de508b4a2235af452",
+    "url": "/spotty/screenshots/genres-page.png"
+  },
+  {
+    "revision": "97457c28daef2f944d1c6347dc3c34c7",
+    "url": "/spotty/screenshots/search-page.png"
+  }
+]);
