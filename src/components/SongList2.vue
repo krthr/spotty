bc884263eaf1804cb45e7aafe4b604f80cd18aa8
krthr <twilson@uninorte.edu.co>
2019-11-23 17:14:28 -0500

    Song list 2

---
+++ src/components/SongList2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <song-list-item-2 v-for="i in 5" :key="i"></song-list-item-2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SongList2",
+
+  components: {
+    SongListItem2: () => import("./SongListItem2.vue")
+  }
+};
+</script>
